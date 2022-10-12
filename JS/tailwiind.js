f9d6e14d49e11eb9be8f0859c11420358ea2c9e8
Michael Anteneh <michael.anteneh@korenticraft.com>
2022-10-12 22:46:00 +0300

    Initial Commit

---
+++ JS/tailwiind.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+      extend: {
+          height: {
+              '400px': '32rem',
+          },
+          width: {
+              '220': '32rem',
+          },colors:{
+            background:"#0b0c10",
+            button:"#f64c72",
+            footer:'#99738e',
+            header:'#66fcf1',
+            text:'#c5c6c7',
+            hover:'#45A29E'
+          }
+      }
+    }
+  }
