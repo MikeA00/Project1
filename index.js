53023d71e6cc8ec8f21071fc59eee60887846f47
Michael Anteneh <michael.anteneh@korenticraft.com>
2022-10-14 22:46:11 +0300

    Adding express and EJS

--- index.js
+++ index.js
@@ -4,178 +4,193 @@ const dbConfig = config.get('Gabi_.dbConfig.dbName');
 const { MongoClient } = require("mongodb");
  
 const client = new MongoClient(dbConfig);
- 
-// The database to use
+
+// // The database to use
 const dbName = "gabi";
 
-// inserting order
- async function insertingOrder() {
-    try {
-        await client.connect();
-        console.log("Connected correctly to server");
+// // inserting order
+//  async function insertingOrder() {
+//     try {
+//         await client.connect();
+//         console.log("Connected correctly to server");
 
-        const db = client.db(dbName);
-         // Using collection order
-        const col = db.collection("order");
+//         const db = client.db(dbName);
+//          // Using collection order
+//         const col = db.collection("order");
 
-         let orderdocument = {
-            //field of order
-             "name": "Abebe Kebede" ,
-             "date": new Date(), 
-             "phoneNumber": "+251962385614",
-             "Destination": "Addis Ababa, Ethiopia",
-             "paid": true,
-             'orderId':"p123",
-         }
-         const p = await col.insertOne(orderdocument);
-        } catch (err) {
-         console.log(err.stack);
-        }
-        finally{
-            await client.close();
-            console.log("Disconnected correctly to server");
-        }
-}
+//          let orderdocument = {
+//             //field of order
+//              "name": "Abebe Kebede" ,
+//              "date": new Date(), 
+//              "phoneNumber": "+251962385614",
+//              "Destination": "Addis Ababa, Ethiopia",
+//              "paid": true,
+//              'orderId':"p123",
+//          }
+//          const p = await col.insertOne(orderdocument);
+//         } catch (err) {
+//          console.log(err.stack);
+//         }
+//         finally{
+//             await client.close();
+//             console.log("Disconnected correctly to server");
+//         }
+// }
 
-//reading order data from DB
-async function readingOrder() {
-    try {
-        await client.connect();
-        console.log("Connected correctly to server");
+// //reading order data from DB
+// async function readingOrder() {
+//     try {
+//         await client.connect();
+//         console.log("Connected correctly to server");
 
-        const db = client.db(dbName);
-         // Using collection order
-        const col = db.collection("order");
+//         const db = client.db(dbName);
+//          // Using collection order
+//         const col = db.collection("order");
 
-         const p = await col.findOne({paid:true});
-         console.log(p);
-        } catch (err) {
-         console.log(err.stack);
-        }
-        finally{
-            await client.close();
-            console.log("Disconnected correctly to server");
-        }
-}
+//          const p = await col.findOne({paid:true});
+//          console.log(p);
+//         } catch (err) {
+//          console.log(err.stack);
+//         }
+//         finally{
+//             await client.close();
+//             console.log("Disconnected correctly to server");
+//         }
+// }
 
-//deleting order from db
-async function deletingOrder() {
-    try {
-        await client.connect();
-        console.log("Connected correctly to server");
+// //deleting order from db
+// async function deletingOrder() {
+//     try {
+//         await client.connect();
+//         console.log("Connected correctly to server");
 
-        const db = client.db(dbName);
-         // Using collection order
-        const col = db.collection("order");
+//         const db = client.db(dbName);
+//          // Using collection order
+//         const col = db.collection("order");
 
-         const p = await col.deleteOne({paid:true});
-        } catch (err) {
-         console.log(err.stack);
-        }
-        finally{
-            await client.close();
-            console.log("Disconnected correctly to server");
-        }
-}
-//inserting product to DB
-async function insertingProduct() {
-    try {
-        await client.connect();
-        console.log("Connected to server");
+//          const p = await col.deleteOne({paid:true});
+//         } catch (err) {
+//          console.log(err.stack);
+//         }
+//         finally{
+//             await client.close();
+//             console.log("Disconnected correctly to server");
+//         }
+// }
+// //inserting product to DB
+// async function insertingProduct() {
+//     try {
+//         await client.connect();
+//         console.log("Connected to server");
 
-         const db = client.db(dbName);
-         // Using collection product
-         const col = db.collection("product");
-         let productDocument = {
-            //field of order
-             "Image": new Date(),
-             "price": "2500",
-             "orderId": "",
-             "Quantity": 7,
-             "price":"p123",
-         }
-         const p = await col.insertOne(productDocument);
-        }catch (err) {
-         console.log(err.stack);
-        }
-        finally{
-            await client.close();
-            console.log("Disconnected correctly to server");
-        }
-}
+//          const db = client.db(dbName);
+//          // Using collection product
+//          const col = db.collection("product");
+//          let productDocument = {
+//             //field of order
+//              "Image": new Date(),
+//              "price": "2500",
+//              "orderId": "",
+//              "Quantity": 7,
+//              "price":"p123",
+//          }
+//          const p = await col.insertOne(productDocument);
+//         }catch (err) {
+//          console.log(err.stack);
+//         }
+//         finally{
+//             await client.close();
+//             console.log("Disconnected correctly to server");
+//         }
+// }
 
-//updating product in DB
-async function updatingProduct() {
-    try {
-        await client.connect();
-        console.log("Connected to server");
+// //updating product in DB
+// async function updatingProduct() {
+//     try {
+//         await client.connect();
+//         console.log("Connected to server");
 
-         const db = client.db(dbName);
-         // Using collection product
-         const col = db.collection("product");
-         let productDocument = {
-            //field of order
-             "Image": new Date(), 
-             "price": "2500",  
-             "orderId": "",
-             "Quantity": 7,
+//          const db = client.db(dbName);
+//          // Using collection product
+//          const col = db.collection("product");
+//          let productDocument = {
+//             //field of order
+//              "Image": new Date(), 
+//              "price": "2500",  
+//              "orderId": "",
+//              "Quantity": 7,
              
-         }
-         const p = await col.updateOne({price:"p123"},{$set:{price:5000}});
-        }catch (err) {
-         console.log(err.stack);
-        }
-        finally{
-            await client.close();
-            console.log("Disconnected correctly to server");
-        }
-}
-//reading product data from DB
-async function readingProduct() {
-    try {
-        await client.connect();
-        console.log("Connected to server");
+//          }
+//          const p = await col.updateOne({price:"p123"},{$set:{price:5000}});
+//         }catch (err) {
+//          console.log(err.stack);
+//         }
+//         finally{
+//             await client.close();
+//             console.log("Disconnected correctly to server");
+//         }
+// }
+// //reading product data from DB
+// async function readingProduct() {
+//     try {
+//         await client.connect();
+//         console.log("Connected to server");
 
-         const db = client.db(dbName);
-         // Using collection product
-         const col = db.collection("product");
-         let productDocument = {
-            //field of order
-             "Image": new Date(), // May 23, 1912                                                                                                                                 
-             "price": "2500",  // May 7, 1954                                                                                                                                  
-             "orderId": "",
-             "Quantity": 7,
-             "price":"p123",
-         }
-         const p = await col.findOne();
-         console.log(p);
-        }catch (err) {
-         console.log(err.stack);
-        }
-        finally{
-            await client.close();
-            console.log("Disconnected correctly to server");
-        }
-}
-//deleting product data from DB
-async function deletingProduct() {
-    try {
-        await client.connect();
-        console.log("Connected to server");
+//          const db = client.db(dbName);
+//          // Using collection product
+//          const col = db.collection("product");
+//          let productDocument = {
+//             //field of order
+//              "Image": new Date(), // May 23, 1912                                                                                                                                 
+//              "price": "2500",  // May 7, 1954                                                                                                                                  
+//              "orderId": "",
+//              "Quantity": 7,
+//              "price":"p123",
+//          }
+//          const p = await col.findOne();
+//          console.log(p);
+//         }catch (err) {
+//          console.log(err.stack);
+//         }
+//         finally{
+//             await client.close();
+//             console.log("Disconnected correctly to server");
+//         }
+// }
+// //deleting product data from DB
+// async function deletingProduct() {
+//     try {
+//         await client.connect();
+//         console.log("Connected to server");
 
-         const db = client.db(dbName);
-         // Using collection product
-         const col = db.collection("product");
+//          const db = client.db(dbName);
+//          // Using collection product
+//          const col = db.collection("product");
 
-         await col.deleteOne({Quantity:7});
+//          await col.deleteOne({Quantity:7});
          
-        }catch (err) {
-         console.log(err.stack);
-        }
-        finally{
-            await client.close();
-            console.log("Disconnected correctly to server");
-        }
-}
-//deletingOrder().catch(console.dir);
-//insertingProduct().catch(console.dir);
+//         }catch (err) {
+//          console.log(err.stack);
+//         }
+//         finally{
+//             await client.close();
+//             console.log("Disconnected correctly to server");
+//         }
+// }
+
+const express =require("express")
+const app = express()
+app.set("view engine", "ejs")
+app.get('/',(req,res)=>{
+    res.render("Home")
+})
+const productRouter= require('./router/Product')
+const deliveryRouter= require('./router/Delivery')
+const about_usRouter= require('./router/about_us')
+
+app.use(express.static('public'));
+app.use('/Product',productRouter)
+app.use('/about_us',about_usRouter)
+app.use('/Delivery',deliveryRouter)
+
+app.listen(3000)
---
+++ public/JS/swipe.js
@@ -0,0 +1,70 @@
+const swiper1 = new Swiper('.swiper1', {
+    
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+    delay: 3000,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+const swiper3 = new Swiper('.swiper3', {
+
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+    delay: 3000,
+    },
+    
+    breakpoints:{
+        0:{
+            slidesPerView:1
+        },700:{
+            slidesPerView:2
+        },991:{
+            slidesPerView:3
+        }
+    },
+    slidesPerView: 3,
+    pagination: {
+        el: '.swiper-pagination',
+        },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+        
+const swiper2 = new Swiper('.swiper2', {
+// Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+    delay: 3000,
+    },
+    
+    breakpoints:{
+        0:{
+            slidesPerView:1
+        },700:{
+            slidesPerView:2
+        },967:{
+            slidesPerView:3
+        }
+    },pagination: {
+        el: '.swiper-pagination',
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
